84bc90be47d784e25414b6efb2fc53d6233cdb31
Abhijeet Vaibhav <66539655+imabhivaibhav@users.noreply.github.com>
2025-11-06 17:44:36 +0530

    Update ai_mode.py

--- ai_mode.py
+++ ai_mode.py
@@ -4,7 +4,7 @@ import requests
 import streamlit as st
 
 HF_TOKEN = st.secrets["HF_TOKEN"]  # your Hugging Face token
-MODEL_ID = "TheBloke/Llama-3.1-8B-Instruct"
+MODEL_ID = "deepseek-ai/DeepSeek-OCR"
 
 # -----------------------------
 # Retrieve top sections based on semantic similarity
@@ -59,3 +59,4 @@ def generate_ai_answer(question, retrieved_sections):
             return f"⚠️ AI generation failed ({response.status_code}): {response.text}"
     except Exception as e:
         return f"⚠️ AI generation error: {str(e)}"
+
